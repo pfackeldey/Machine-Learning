d5ca7ea5c10243021c51279665b5f072c021de1f
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2017-10-13 16:21:38 +0200

    removed old files

--- scikitlearnclassification.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from python.scikitlearnclassification import scikit_classification
-import sys
-
-if __name__ == "__main__" and len(sys.argv) > 1:
-	scikit_classification()
