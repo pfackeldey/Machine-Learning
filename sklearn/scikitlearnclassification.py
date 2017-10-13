6d6b7ab5c6e0d6fe41fa9e2f9ac1b206999e74aa
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2017-10-13 16:18:21 +0200

    changed structure of sklearn folder

---
+++ sklearn/scikitlearnclassification.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from python.scikitlearnclassification import scikit_classification
+import sys
+
+if __name__ == "__main__" and len(sys.argv) > 1:
+	scikit_classification()
