57c9128108815026cf6d1353eb0e175400b69920
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-10-14 14:59:48 +0200

    mainly bug fixes and styling

--- keras/train.py
+++ keras/train.py
@@ -6,7 +6,7 @@ import ROOT
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-from model import KerasModel
+from model import KerasModels
 from treetools import TreeExtender
 
 import argparse
@@ -14,50 +14,60 @@ import os
 import sys
 import yaml
 
-EPOCHS = 10
-BATCH_SIZE = 128
+EPOCHS = 3
+BATCH_SIZE = 256
 
 
-def multiclassNeuralNetwork(args_from_script=None):
+def classificationNeuralNetwork(args_from_script=None):
 
+    parser = argparse.ArgumentParser(description="Perform binary classification NN training with kPyKeras (TMVA).",
+                                     fromfile_prefix_chars="@", conflict_handler="resolve")
 
-	parser = argparse.ArgumentParser(description="Perform Multiclass NN training with kPyKeras (TMVA).",
-	                                 fromfile_prefix_chars="@", conflict_handler="resolve")
+    parser.add_argument("config", help="Path to training config")
+    args = parser.parse_args()
 
-	parser.add_argument("config", help = "Path to training config")
-	args = parser.parse_args()
+    ROOT.TMVA.Tools.Instance()
+    ROOT.TMVA.PyMethodBase.PyInitialize()
 
-	ROOT.TMVA.Tools.Instance()
-	ROOT.TMVA.PyMethodBase.PyInitialize()
+    # initialize factory:
 
-	# initialize factory:
-	
-	factory = ROOT.TMVA.Factory("TMVAMulticlass", ROOT.TFile.Open("training_output.root", "RECREATE"), "!V:!Silent:Color:!DrawProgressBar:Transformations=None:AnalysisType=multiclass")
-	
-	# load yaml training config
-	config = yaml.load(open(args.config), "r")
+    factory = ROOT.TMVA.Factory("TMVAclassificatoin", ROOT.TFile.Open("training_output.root", "RECREATE"),
+                                "!V:!Silent:Color:!DrawProgressBar:Transformations=None:AnalysisType=Classification")
 
-	dataloader = ROOT.TMVA.DataLoader("example_training")
-	
-	for variable in config["variables"]:
-		dataloader.AddVariable(variable)
-	trees = {}
-	for class_ in config["classes"]:
-		trees[class_] = ROOT.TFile(config["training_data"]).Get(class_)
-		dataloader.AddTree(trees[class_], class_, config["class_weights"][class_]*config["global_weight"])
-		dataloader.SetWeightExpression(config["event_weight"], class_)
-		dataloader.PrepareTrainingAndTestTree(Root.TCut(""), "TrainTestSplit_{}={}:".format(class_, config["train_test_split"])+"SplitMode=Random:NormMode=None")
+    # load yaml training config
+    config = yaml.load(open(args.config, "r"))
 
-	model = KerasModel(n_features = len(config["features"]), n_classes = len(config["classes"]), learning_rate = 0.1)
-	model.example_model()
-	model.summary()
-	model.save("example_model.h5")
+    dataloader = ROOT.TMVA.DataLoader("example_training")
 
-	factory.BookMethod(dataloader, ROOT.TMVA.Types.kPyKeras, "PyKeras_example", "!H:!V:VarTransform=None:FileNameModel=example_model.h5:SaveBestOnly=true:TriesEarlyStopping=-1:NumEpochs={}:".format(EPOCHS)+"BatchSize={}".format(BATCH_SIZE))
+    for signal_, signal_weight in zip(config["signal_inputs"], config["signal_weights"]):
+        signal = ROOT.TFile.Open(signal_)
+        signal_weight = signal_weight * config["global_weight"]
+        dataloader.AddSignalTree(
+            signal.Get("tree"), signal_weight)
 
-	factory.TrainAllMethods()
-	factory.TestAllMethods()
-	factory.EvaluateAllMethods()
+    for background_, background_weight in zip(config["background_inputs"], config["background_weights"]):
+        background = ROOT.TFile.Open(background_)
+        background_weight = background_weight * config["global_weight"]
+        dataloader.AddBackgroundTree(
+            background.Get("tree"), background_weight)
+
+    for feature in config["features"]:
+        dataloader.AddVariable(feature)
+
+    dataloader.PrepareTrainingAndTestTree(
+        ROOT.TCut(""), config["train_test_split"])
+
+    model = KerasModels(n_features=len(config["features"]), n_classes=len(
+        config["classes"]), learning_rate=0.001)
+    model.example_model()
+
+    factory.BookMethod(dataloader, ROOT.TMVA.Types.kPyKeras, "PyKeras_example",
+                       "!H:!V:VarTransform=None:FileNameModel=example_model.h5:SaveBestOnly=true:TriesEarlyStopping=-1:NumEpochs={}:".format(EPOCHS) + "BatchSize={}".format(BATCH_SIZE))
+
+    factory.TrainAllMethods()
+    factory.TestAllMethods()
+    factory.EvaluateAllMethods()
+
 
 if __name__ == "__main__" and len(sys.argv) > 1:
-	multiclassNeuralNetwork()
+    classificationNeuralNetwork()
