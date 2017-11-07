34424e368c4cad96160891b77aa70ee538bf3baf
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-11-07 11:01:44 +0100

    added multiclass GUI

--- runTMVAGui.py
+++ runTMVAGui.py
@@ -5,9 +5,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Start TMVA Graphic User Interface.",
                                  fromfile_prefix_chars="@", conflict_handler="resolve")
-
 parser.add_argument("-i", "--input", help="Path to TMVA output", required=True)
+parser.add_argument("--multiclass", default=False, action='store_true',
+                    help="Run multiclass GUI instead of binary classification GUI. [Default: %(default)s]")
 args = parser.parse_args()
 
-ROOT.TMVA.TMVAGui(args.input)
+if args.multiclass:
+    ROOT.TMVA.TMVAMultiClassGui(args.input)
+else:
+    ROOT.TMVA.TMVAGui(args.input)
 raw_input("Press Enter to exit")
