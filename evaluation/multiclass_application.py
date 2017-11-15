0153f4d35e6f062f819c365dd117b19d3b781243
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-11-15 12:50:52 +0100

    update on max response branch application

--- evaluation/multiclass_application.py
+++ evaluation/multiclass_application.py
@@ -24,30 +24,62 @@ def multiclass_application():
     ROOT.TMVA.Tools.Instance()
     ROOT.TMVA.PyMethodBase.PyInitialize()
 
-    # initialize TMVA reader
-    reader = ROOT.TMVA.Reader('Color:!Silent')
-
-    # load config with training features and weight files...
     config = yaml.load(open(args.config, "r"))
 
-    # add features to TMVA reader
+    # Initialize TMVA Reader and book variables
+    reader = ROOT.TMVA.Reader('Color:!Silent')
     values = {}
-    for feature in config["features"]:
-        values[feature] = array("f", [-999])
-        reader.AddVariable(feature, values[feature])
+    for variable in config["features"]:
+        values[variable] = array("f", [-999])
+        reader.AddVariable(variable, values[variable])
 
-    # book the classification method (trainings weight file)
-    reader.BookMVA(config["trainings_weight_file"],
-                   config["trainings_weight_file"])
+    # Book methods for classification of different folds
+    classifiers = config["trainings_weight_file"]
+    for classifier in classifiers:
+        reader.BookMVA(ROOT.TString(classifier), ROOT.TString(classifier))
 
-    # add new branch with TreeExtender
-    dst = config["target_file"]
-    for file_ in config["source_files"]:
-        with TreeExtender(file_, dst) as te:
-            te.addBranch("PyKeras_MSSM_HWW", unpackBranches=["event"])
-            for entry in te:
-                response = reader.EvaluateMulticlass(
-                    config["trainings_weight_file"][event % 10 >= 4])
+    # Open input file and register branches with input and output variables
+    files = config["source_files"]
+    for f in files:
+        file_ = ROOT.TFile(f, "UPDATE")
+        if file_ == None:
+            raise Exception("File is not existent: {}".format(f))
+
+        tree = file_.Get('latino')
+        if tree == None:
+            raise Exception("Tree {} is not existent in file: {}".format(
+                'latino', f))
+
+        for feature in config["features"]:
+            tree.SetBranchAddress(feature, values[featuree])
+
+        response_branches = []
+        response_single_scores = []
+        prefix = 'NN_score_'
+        for class_ in config["classes"]:
+            response_single_scores.append(array("f", [-999]))
+            response_branches.append(
+                tree.Branch("{}{}".format(prefix, class_), response_single_scores[
+                    -1], "{}{}/F".format(prefix, class_)))
+
+        response_max_score = array("f", [-999])
+        response_branches.append(
+            tree.Branch("{}max_score".format(prefix), response_max_score,
+                        "{}max_score/F".format(prefix)))
+
+        response_max_index = array("f", [-999])
+        response_branches.append(
+            tree.Branch("{}max_index".format(prefix), response_max_index,
+                        "{}max_index/F".format(prefix)))
+
+        # Loop over events and add method's response to tree
+        for i_event in range(tree.GetEntries()):
+            # Get current event
+            tree.GetEntry(i_event)
+
+            # Get event number and calculate method's response
+            event = int(getattr(tree, config["event_branch"]))
+            response = reader.EvaluateMulticlass(classifiers[event % 2])
 
             # Find max score and index
             response_max_score[0] = -999.0
@@ -57,7 +89,13 @@ def multiclass_application():
                     response_max_score[0] = r
                     response_max_index[0] = i
 
-            entry.PyKeras_MSSM_HWW[0] = response_max_score[0]
+            # Fill branches
+            for branch in response_branches:
+                branch.Fill()
+
+        # Write new branches to input file
+        file_.Write()
+        file_.Close()
 
 
 if __name__ == "__main__" and len(sys.argv) > 1:
