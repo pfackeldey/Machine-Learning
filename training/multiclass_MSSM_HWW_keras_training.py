c459879bcaf3e5fa3e3a238d9d1b9512d82e797c
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-12-01 14:50:59 +0100

    bugfix

--- training/multiclass_MSSM_HWW_keras_training.py
+++ training/multiclass_MSSM_HWW_keras_training.py
@@ -100,9 +100,9 @@ def multiclassNeuralNetwork(args_from_script=None):
 
     model = KerasModels(n_features=len(config["features"]), n_classes=len(
         config["classes"]), learning_rate=args.learning_rate, plot_model=False, modelname="multiclass_model_fold{}.h5".format(args.fold))
-    model.multiclass_MSSM_HWW_model()
+    multiclass_model = getattr(model.multiclass_MSSM_HWW_model())
 
-    model.model.fit(
+    multiclass_model.fit(
         x_train,
         y_train,
         sample_weight=w_train,
