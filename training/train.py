acefd8770617e20a9e473e56713c7e1636358955
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-12-18 18:58:10 +0100

    renamed

--- training/train.py
+++ training/train.py
@@ -1,77 +1,104 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
+import numpy as np
+np.random.seed(1234)
+
+from collections import Counter
+import argparse
+import yaml
 import os
 import sys
-import ROOT
 
-ROOT.PyConfig.IgnoreCommandLineOptions = True
+from sklearn import model_selection
 
 base = os.path.normpath(os.path.join(os.path.abspath(__file__), "../.."))
 sys.path.append(base)
 
 from utils.model import KerasModels
-from utils.treetools import TreeExtender
 
-import argparse
-import os
-import sys
-import yaml
-
-EPOCHS = 20
-BATCH_SIZE = 64
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 
 
-def classificationNeuralNetwork(args_from_script=None):
+def multiclassNeuralNetwork(args_from_script=None):
 
-    parser = argparse.ArgumentParser(description="Perform binary classification NN training with kPyKeras (TMVA).",
+    parser = argparse.ArgumentParser(description="Perform multiclassification NN training with Keras.",
                                      fromfile_prefix_chars="@", conflict_handler="resolve")
-
+    parser.add_argument("--fold", default=0, choices=[0, 1],
+                        help="Training fold. [Default: %(default)s]")
+    parser.add_argument("--epochs", default=20,
+                        help="Number of training epochs. [Default: %(default)s]")
+    parser.add_argument("--learning-rate", default=0.0001,
+                        help="Learning rate of NN. [Default: %(default)s]")
+    parser.add_argument("--batch-size", default=100000,
+                        help="Batch size for training. [Default: %(default)s]")
+    parser.add_argument("--early-stopping", default=False, action='store_true',
+                        help="Stop training if loss increases again. [Default: %(default)s]")
     parser.add_argument("config", help="Path to training config")
     args = parser.parse_args()
 
-    ROOT.TMVA.Tools.Instance()
-    ROOT.TMVA.PyMethodBase.PyInitialize()
-
-    # initialize factory:
-
-    factory = ROOT.TMVA.Factory("TMVAclassificatoin", ROOT.TFile.Open("training_output.root", "RECREATE"),
-                                "!V:!Silent:Color:!DrawProgressBar:Transformations=None:AnalysisType=Classification")
-
-    # load yaml training config
     config = yaml.load(open(args.config, "r"))
 
-    dataloader = ROOT.TMVA.DataLoader("example_training")
+    folder = '/home/mf278754/master/arrays/'
 
-    for signal_, signal_weight in zip(config["signal_inputs"], config["signal_weights"]):
-        signal = ROOT.TFile.Open(signal_)
-        signal_weight = signal_weight * config["global_weight"]
-        dataloader.AddSignalTree(
-            signal.Get("tree"), signal_weight)
+    # load trainings data and weights
+    x = np.load(folder + 'x_fold{}.npy'.format(args.fold))
+    y = np.load(folder + 'y_fold{}.npy'.format(args.fold))
+    w = np.load(folder + 'weights_fold{}.npy'.format(args.fold))
+    w = w * config["global_weight"]
 
-    for background_, background_weight in zip(config["background_inputs"], config["background_weights"]):
-        background = ROOT.TFile.Open(background_)
-        background_weight = background_weight * config["global_weight"]
-        dataloader.AddBackgroundTree(
-            background.Get("tree"), background_weight)
 
-    for feature in config["features"]:
-        dataloader.AddVariable(feature)
 
-    dataloader.PrepareTrainingAndTestTree(
-        ROOT.TCut(""), config["train_test_split"])
+    # Split data in training and testing
+    x_train, x_test, y_train, y_test, w_train, w_test = model_selection.train_test_split(
+        x, y, w, test_size=1.0 - config["train_test_split"], random_state=1234)
+
+    def get_class_weights(y):
+        counter = Counter(y)
+        majority = 1. #max(counter.values())
+        return  {cls: float(majority/count) for cls, count in counter.items()}
+
+    # Add callbacks
+    callbacks = []
+    # callbacks.append(TensorBoard(log_dir='/home/mf278754/master/logs',
+    #                             histogram_freq=1, write_graph=True, write_images=True))
+    callbacks.append(
+        ModelCheckpoint(filepath="/home/mf278754/master/fold{}_multiclass_model.h5".format(args.fold), save_best_only=True, verbose=1))
+    if args.early_stopping:
+        callbacks.append(EarlyStopping(monitor='val_loss',
+                                       min_delta=0,
+                                       patience=2,
+                                       verbose=0, mode='auto'))
+
+    # preprocessing
+    from sklearn import preprocessing
+    scaler = preprocessing.StandardScaler().fit(x_train)
+    x_train_scaled = scaler.transform(x_train)
+
+    scaler = preprocessing.StandardScaler().fit(x_test)
+    x_test_scaled = scaler.transform(x_test)
 
     model = KerasModels(n_features=len(config["features"]), n_classes=len(
-        config["classes"]), learning_rate=0.0005, plot_model=True)
-    model.example_model()
+        config["classes"]), learning_rate=args.learning_rate, plot_model=False, modelname="multiclass_model_fold{}.h5".format(args.fold))
+    keras_model = model.multiclass_MSSM_HWW_model()
+    fit = keras_model.fit(
+        x_train_scaled,
+        y_train,
+        class_weight=get_class_weights(np.argmax(y_train, axis=-1)),
+        validation_data=(x_test_scaled, y_test),
+        batch_size=args.batch_size,
+        epochs=args.epochs,
+        shuffle=True,
+        callbacks=callbacks)
 
-    factory.BookMethod(dataloader, ROOT.TMVA.Types.kPyKeras, "PyKeras_example",
-                       "!H:!V:VarTransform=None:FileNameModel=example_model.h5:SaveBestOnly=true:TriesEarlyStopping=-1:NumEpochs={}:".format(EPOCHS) + "BatchSize={}".format(BATCH_SIZE))
+    folder_result = '/home/mf278754/master/results/'
+    if not os.path.exists(folder_result):
+        os.makedirs(folder_result)
 
-    factory.TrainAllMethods()
-    factory.TestAllMethods()
-    factory.EvaluateAllMethods()
-
+    # dump loss and accuracy to numpy arrays
+    np.save(folder_result + 'loss.npy', fit.history["loss"])
+    np.save(folder_result + 'val_loss.npy', fit.history["val_loss"])
+    np.save(folder_result + 'acc.npy', fit.history["acc"])
+    np.save(folder_result + 'val_acc.npy', fit.history["val_acc"])
 
 if __name__ == "__main__" and len(sys.argv) > 1:
-    classificationNeuralNetwork()
+    multiclassNeuralNetwork()
