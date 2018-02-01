f5c884651569bca59784688c1ae565fdbf396cdc
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2018-02-01 16:49:18 +0100

    added git clone and luigi installation

---
+++ checkout_script.sh
@@ -0,0 +1,9 @@
+# clone repository
+git clone https://github.com/CMSAachen3B/Machine-Learning.git
+
+# export path for python packages
+export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
+
+# install more python packages
+pip install luigi --user --upgrade
+
