5b09f2167502f93270137bf373728f111a75c550
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2018-02-15 16:03:10 +0100

    changed to merged classes

--- run_application.sh
+++ run_application.sh
@@ -1,8 +1,7 @@
-array=("hww120_200" "hww210_500" "hww550_3000" "DY" "WW" "VVV" "DiBoson" "WJets" "Top" "data") 
-for i in "${array[@]}" 
+array=("hww120_200" "hww210_500" "hww550_3000" "DY" "WW" "misc" "Top" "data") 
+for i in "${array[@]}"
 do
 	echo "RUNNING: python evaluation/keras_evaluation.py config/MSSM_HWW.yaml --tree ${i}"
 	echo "Processing tree: ${i}"
 	python evaluation/keras_evaluation.py config/MSSM_HWW.yaml --tree ${i}
 done
-
