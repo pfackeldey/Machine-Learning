1426d7684290e32bacebc50a41f5ab33d0e75e2b
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2018-02-15 16:01:44 +0100

    changed to process individual ntuples

--- evaluation/keras_evaluation.py
+++ evaluation/keras_evaluation.py
@@ -9,6 +9,7 @@ from array import array
 import yaml
 import pickle
 import numpy as np
+import os
 
 from keras.models import load_model
 
@@ -23,81 +24,85 @@ def keras_evaluation(args_from_script=None):
 	config = yaml.load(open(args.config, "r"))
 
 
-
 	# Load keras model and preprocessing
 	classifiers = []
-	for c in config["classifiers"]:
+	preprocessing = []
+	for c, p in zip(config["classifiers"], config["preprocessing"]):
 		classifiers.append(load_model(c))
-	# inverted hierarchy
-	input_file = "/net/scratch_cms3b/fackeldey/trainingssets/full_MSSM_HWW_trainingsset.root"
+		preprocessing.append(pickle.load(open(p, "rb")))
 
-	# Open input file and register branches with input and output variables
-	file_ = ROOT.TFile(input_file, "update")
-	if file_ == None:
-		raise Exception(
-			"File is not existent: {}".format(input_file))
+	for process in config["processes"]:
+		print "************* PROCESS: {} *************".format(process)
 
-	tree = file_.Get(args.tree)
-	if tree == None:
-		raise Exception("Tree {} is not existent in file: {}".format(args.tree, input_file))
+		for filename in config["processes"][process]["files"]:
+			path = os.path.join(config["base_path"], filename)
 
-	values = []
-	for feature in config["features"]:
-		values.append(array("f", [-999]))
-		tree.SetBranchAddress(feature, values[-1])
+			print "Currently processing {}".format(path)
 
-	response_branches = []
-	response_single_scores = []
-	prefix = config["branch_prefix"]
-	for class_ in config["classes"]:
-		response_single_scores.append(array("f", [-999]))
-		response_branches.append(
-			tree.Branch("{}{}".format(prefix, class_),
-						response_single_scores[-1], "{}{}/F".format(
-						    prefix, class_)))
+			# Open input file and register branches with input and output variables
+			file_ = ROOT.TFile(path, "update")
+			if file_ == None:
+				raise Exception(
+					"File is not existent: {}".format(path))
 
-	response_max_score = array("f", [-999])
-	response_branches.append(
-	tree.Branch("{}max_score".format(prefix), response_max_score,
-			    "{}max_score/F".format(prefix)))
+			tree = file_.Get(args.tree)
+			if tree == None:
+				raise Exception("Tree {} is not existent in file: {}".format(args.tree, path))
 
-	response_max_index = array("f", [-999])
-	response_branches.append(
-	tree.Branch("{}max_index".format(prefix), response_max_index,
-			    "{}max_index/F".format(prefix)))
+			values = []
+			for feature in config["features"]:
+				values.append(array("f", [-999]))
+				tree.SetBranchAddress(feature, values[-1])
 
-	# Loop over events and add method's response to tree
-	for i_event in range(tree.GetEntries()):
-		# Get current event
-		tree.GetEntry(i_event)
+			response_branches = []
+			response_single_scores = []
+			prefix = config["branch_prefix"]
+			for class_ in config["classes"]:
+				response_single_scores.append(array("f", [-999]))
+				response_branches.append(
+					tree.Branch("{}{}".format(prefix, class_),
+								response_single_scores[-1], "{}{}/F".format(
+								    prefix, class_)))
 
-		# Get event number and calculate method's response
-		event = int(getattr(tree, config["event_branch"]))
-		values_stacked = np.hstack(values).reshape(1, len(values))
+			response_max_score = array("f", [-999])
+			response_branches.append(
+			tree.Branch("{}max_score".format(prefix), response_max_score,
+					    "{}max_score/F".format(prefix)))
 
-		# preprocessing
-		#from sklearn import preprocessing
-		#scaler = preprocessing.StandardScaler().fit(values_stacked)
-		#values_preprocessed = scaler.transform(values_stacked)
+			response_max_index = array("f", [-999])
+			response_branches.append(
+			tree.Branch("{}max_index".format(prefix), response_max_index,
+					    "{}max_index/F".format(prefix)))
 
-		response = classifiers[event % 2].predict(values_stacked)
-		response = np.squeeze(response)
+			# Loop over events and add method's response to tree
+			for i_event in range(tree.GetEntries()):
+				# Get current event
+				tree.GetEntry(i_event)
 
-		# Find max score and index
-		response_max_score[0] = -999.0
-		for i, r in enumerate(response):
-			response_single_scores[i][0] = r
-			if r > response_max_score[0]:
-				response_max_score[0] = r
-				response_max_index[0] = i
+				# Get event number and calculate method's response
+				event = int(getattr(tree, config["event_branch"]))
+				values_stacked = np.hstack(values).reshape(1, len(values))
 
-		# Fill branches
-		for branch in response_branches:
-			branch.Fill()
+				# preprocessing
+				values_preprocessed = preprocessing[event % 2].transform(values_stacked)
+				response = classifiers[event % 2].predict(values_preprocessed)
+				response = np.squeeze(response)
 
-	# Write new branches to input file
-	tree.Write("", ROOT.TObject.kOverwrite)
-	file_.Close()
+				# Find max score and index
+				response_max_score[0] = -999.0
+				for i, r in enumerate(response):
+					response_single_scores[i][0] = r
+					if r > response_max_score[0]:
+						response_max_score[0] = r
+						response_max_index[0] = i
+
+				# Fill branches
+				for branch in response_branches:
+					branch.Fill()
+
+			# Write new branches to input file
+			tree.Write("", ROOT.TObject.kOverwrite)
+			file_.Close()
 
 
 if __name__ == "__main__":
