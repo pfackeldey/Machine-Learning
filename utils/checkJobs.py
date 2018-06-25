fdbe3aeb27a566351eb9adfa864c3cf912041138
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2018-06-25 18:19:34 +0200

    fixed print outs

--- utils/checkJobs.py
+++ utils/checkJobs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import os
+import re
 import glob
 import argparse
 
 
-def checkJobs():
+def checkJobs(args_from_script=None):
+
     parser = argparse.ArgumentParser(description="Check if job was successful.",
                                      fromfile_prefix_chars="@", conflict_handler="resolve")
     parser.add_argument("--log-dir", help="path to condor logs")
@@ -15,8 +17,9 @@ def checkJobs():
 
     for _file in files:
         if "Traceback" in open(_file).read():
-            print _file, "job failed..."
-            continue
+            print "\033[91mJob {} failed!\033[0m".format(re.search('part(.+?).error', _file).group(1))
+        else:
+            print "\033[1;32mJob {} was successful!\033[1;m".format(re.search('part(.+?).error', _file).group(1))
 
 
 if __name__ == "__main__":
